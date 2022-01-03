c9d6541ea084b9cb6b2889db56e92c2110cb4b47
digang <tk119994@gmail.com>
2022-01-03 17:53:12 +0900

    Matching Button
    Add matching Button but i didn't make wait comment .

--- Randomchat/chat/src/App.js
+++ Randomchat/chat/src/App.js
@@ -5,19 +5,23 @@ import io from 'socket.io-client';
 
 const socket = io.connect('http://localhost:4000')
 
-
 function App() {
 
   const [state, setState] = useState({message:''})
   const [chat,setChat] =useState([])
-
+  const [isbtnClicked,setClick] = useState(false)
 
   useEffect(()=>{
     socket.on('message',({name,message})=>{
       console.log(message)
       setChat([...chat,{name, message}])
     })
-  })
+
+    socket.on('update_toID', (ID) =>{
+      console.log('is it right?')
+      socket.emit('updateID', ID)
+    })
+  },[chat])
 
   const rendermyChat = ()=>{
     return chat.map( ({name,message}, index)=> (
@@ -26,12 +30,7 @@ function App() {
   }
 
   const onMessageSubmit = (e) =>{
-    // 해야할것 
-    // 1. 내가 전송할 시 li 하나 추가하기 
-    // 2. 내가 전송하고 난뒤 서버에 메세지보내서, 브로드캐스트 하기.
 
-    // 3. 매칭하기.
-    // 4. 매칭해서 연결하기..? 시발 어려워~~!~!~!
     e.preventDefault()
     const {message}= state
     socket.emit('message', {message})
@@ -42,10 +41,31 @@ function App() {
 
   const onChange = (e) =>{
     console.log(e.target.value)
-    const mes = e.target.value
     setState({message : e.target.value})
     
   }
+
+  const renderBtn = ()=>{
+    if(isbtnClicked === true){
+      return (
+        <form action="" id="sendForm" onSubmit={onMessageSubmit}>
+          <input autoComplete="off" onChange={onChange} value={state.message}/><button>전송</button>
+        </form>
+      )
+    }
+    else{
+      return(
+        <button id="findBtn" onClick={match_Btn_click}>매칭 상대 찾기</button>
+      )
+    }
+  }
+
+  const match_Btn_click = (e)=>{
+    socket.emit('findMatch')
+    setClick(true)
+  }
+
+  
 
   return (
     <div className="App">
@@ -60,9 +80,7 @@ function App() {
           <ul id="chatLog">
             {rendermyChat()}
           </ul>
-            <form action="" id="sendForm" onSubmit={onMessageSubmit}>
-              <input autocomplete="off" onChange={onChange} value={state.message}/><button>전송</button>
-            </form>
+            {renderBtn()}
         </div>
       </header>
     </div>
